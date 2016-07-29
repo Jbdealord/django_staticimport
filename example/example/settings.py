41efa3ecd4c335917d2749e79434e99d5c272083
Leonardo Esparis <leoxnidas.c.14@gmail.com>
2016-07-28 22:32:48 -0400

    13# updating example

--- example/example/settings.py
+++ example/example/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+HOSTED_LIBS = [
+    {
+        'name': 'selectjs',
+        'url': 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js'
+    },
+]
