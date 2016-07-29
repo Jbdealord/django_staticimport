e31808b2a0343f160403fcdb6c600ee1e5e3e7ff
Leonardo Esparis <leoxnidas.c.14@gmail.com>
2016-07-28 23:21:32 -0400

    28# updating long description

--- setup.py
+++ setup.py
@@ -58,14 +58,14 @@ HOSTED_LIBS = [
     {% import 'main.css' %}
     {% import 'something.css' %}
     
-    <!-- including js files with custom attributes -->
+    <!-- including js files with attrs attributes -->
     {% import 'angularjs' %}
     {% import 'selectjs' %}
-    {% import 'main.js' custom='async' %}
+    {% import 'main.js' attrs='async' %}
 </head>
 <body>
     <!-- including images -->
-    {% import 'image.jpg' css='image' id='image' custom='width="100px" height="100px" data-city="picture"' %}
+    {% import 'image.jpg' css='image' id='image' attrs='width="100px" height="100px" data-city="picture"' %}
     <h1>hello world</h1>
 </body>
 </html>
