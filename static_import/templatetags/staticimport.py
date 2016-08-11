0ce60fbbc6bc2002f09160a715111278cec954ae
Leonardo Esparis <leoxnidas.c.14@gmail.com>
2016-08-10 23:13:58 -0400

    38# updating staticimport.py to have better performance

--- static_import/templatetags/staticimport.py
+++ static_import/templatetags/staticimport.py
@@ -3,7 +3,7 @@ from django import template
 from static_import.base import (get_static_file as _gf,
                                 is_css, is_js, is_img,
                                 is_remote, is_local, 
-                                safe_extra_attrs)
+                                validate_attrs, guess_tag)
 
 
 register = template.Library()
@@ -13,7 +13,7 @@ register = template.Library()
 def _import(name, **attr):
     fn, wt = _gf(name)
 
-    rs = {
+    _r = {
         'filename': fn,
         'is_css': is_css(fn),
         'is_js': is_js(fn),
@@ -22,7 +22,12 @@ def _import(name, **attr):
         'is_remote': is_remote(wt)
     }
 
-    if len(attr) >= 1:
-        rs.update(safe_extra_attrs(attr))
+    if len(attr) >= 1 \
+       and validate_attrs(attr, guess_tag(fn)):
 
-    return rs
+        if attr.get('style', None):
+            attr['style'] = attr['style'].replace('"', '\'')
+
+        _r.update(attr)
+
+    return _r
